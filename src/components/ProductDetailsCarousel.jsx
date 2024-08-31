0de281c9333a18f96d0224068a8f8c0106f2a1eb
Sunthorn Ruckani <sunthorn.rk@gmail.com>
2024-08-31 23:08:26 +0700

    make product detail page

---
+++ src/components/ProductDetailsCarousel.jsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import {Carousel} from "react-responsive-carousel";
+
+const ProductDetailsCarousel = ({images}) => {
+  return (
+    <div className="text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]">
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showIndicators={false}
+        showStatus={false}
+        thumbWidth={60}
+        className="productCarousel"
+      >
+        <img src="../products/image_1.png" />
+        <img src="../products/image_1.png" />
+        <img src="../products/image_1.png" />
+        <img src="../products/image_1.png" />
+      </Carousel>
+    </div>
+  );
+};
+
+export default ProductDetailsCarousel;
